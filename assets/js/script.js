c7f84f6b22045156c4da118d1591670a834d1738
lweine01 <laurarweiner@gmail.com>
2021-08-29 13:12:12 -0400

    Created search button event listener. Pulled data from weather API to add appropriate text to the site for current date weather.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 const APIKey = "1719704d58808aa20b40d8e2bf9e06ee";
 var cityName = "charlotte";
+var cityMain = document.getElementById("cityMain");
+var searchBtn = document.getElementById("searchBtn");
+var recentSearch = document.getElementById("recentSearch");
+var searchedCity = document.getElementById("searched");
+var currentTemp = document.getElementById("currentTemp");
+var currentWind = document.getElementById("currentWind");
+var currentHum = document.getElementById("currentHum");
+var currentUV = document.getElementById("currentUV");
+var currentDate = document.getElementById("currentDate");
+var currentPic = document.getElementById ("currentPic");
 
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+searchBtn.addEventListener ("click", function(){
 
-function getWeather (){
     var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&appid=" + APIKey;
 
     fetch(requestURL)
@@ -13,7 +22,21 @@ function getWeather (){
     .then (function(data){
         console.log(data);
 
-    })
+        // var searchedList = document.createElement('li');
+        // var userUrl = document.createElement('p');
 
-}
-getWeather();
+        cityMain.textContent = data.name;
+        console.log (data.weather[0].icon);
+        currentPic.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+        var currentFTemp = Math.floor(((data.main.temp - 273.15) * 9 / 5) + 32);
+        currentTemp.textContent = "Temp: " + currentFTemp + " °F"
+        currentWind.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+        currentHum.textContent = "Humidity: " + data.main.humidity + "%";
+        // currentUV.textContent = "UV Index: " + data.daily.humidity;
+
+
+        // usersContainer.append(userName);
+        // usersContainer.append(userUrl);
+    
+    });
+});
