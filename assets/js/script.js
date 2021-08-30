c6a4a60c83cd5a0c0e2ded603485f0f3ef325a14
lweine01 <laurarweiner@gmail.com>
2021-08-30 19:24:55 -0400

    Added 3rd API for 5 day forecast. Wrote for loop to add textContent to the page. Not currently working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,19 @@ var currentTemp = document.getElementById("currentTemp");
 var currentWind = document.getElementById("currentWind");
 var currentHum = document.getElementById("currentHum");
 var currentUV = document.getElementById("currentUV");
-var currentDate = document.getElementById("currentDate");
-
+var fiveDayMain = document.querySelectorAll(".fiveDayMain");
+var fiveDayTemp = document.querySelectorAll(".fiveDayTemp");
+var fiveDayWind = document.querySelectorAll(".fiveDayWind");
+var fiveDayHum = document.querySelectorAll(".fiveDayHum");
+var fiveDayPic = document.querySelectorAll(".fiveDayPic");
 
 function forcast() {
     
     var cityName = searchedCity.value.trim();
-    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
     
-    fetch(requestURL)
+    fetch(currentURL)
     .then(function (response) {
         return response.json();
     })
@@ -23,13 +27,15 @@ function forcast() {
         console.log(data);
         
         // var searchedList = document.createElement('li');
-        
 
-
+        var currentDate = new Date(data.dt*1000);
+        var currentMonth = currentDate.getMonth()+1;
+        var currentDay = currentDate.getDate();
+        var currentYear = currentDate.getFullYear();
         var currentPic = document.createElement("img");
         var currentFTemp = Math.floor(((data.main.temp - 273.15) * 9 / 5) + 32);
         
-        cityMain.innerHTML = data.name;
+        cityMain.textContent = data.name + " " + currentMonth + "/" + currentDay + "/" + currentYear;
         currentPic.alt = data.weather[0].icon;
         currentPic.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
         cityMain.appendChild(currentPic);
@@ -67,6 +73,34 @@ function forcast() {
             
         });
     });
+
+    fetch(fiveDayURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        for (var i=0; i<5; i++) {
+        var daily = daily[i];
+        var fiveDayDate = new Date (data.daily.dt*1000);
+        var fiveDayMonth = fiveDayDate.getMonth()+1;
+        var fiveDayDay= fiveDayDate.getDate();
+        var fiveDayYear = fiveDayDate.getFullYear();
+
+        var fiveDayFTemp = Math.floor(((data.daily.temp.max - 273.15) * 9 / 5) + 32);
+        
+        fiveDayMain.textContent = fiveDayMonth + "/" + fiveDayDay + "/" + fiveDayYear;
+        fiveDayPic.alt = data.daily.weather[0].icon;
+        fiveDayPic.src = "http://openweathermap.org/img/wn/" + data.daily.weather[0].icon + "@2x.png";
+        fiveDayMain.appendChild(currentPic);
+        
+        fiveDayTemp.textContent = "Temp: " + fiveDayFTemp + " °F"
+        currentWind.textContent = "Wind Speed: " + data.daily.wind_speed + " MPH";
+        currentHum.textContent = "Humidity: " + data.daily.humidity + "%";
+        }
+    })
+
 }
 
 searchBtn.addEventListener("click", function (event) {
